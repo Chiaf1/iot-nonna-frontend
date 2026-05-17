26abefd0da339447288bf8592cbac6540560a78d
Daniele Chiaf <28843028+Chiaf1@users.noreply.github.com>
2026-05-17 18:51:49 +0200

    added alert dialog for delte buttons
    Added the alert dialog for delete button with shadcn/ui

--- components/ui_personal/DeleteButton.tsx
+++ components/ui_personal/DeleteButton.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { Button } from "../ui/button";
+
 // Accetta come props una funzione asyncrona sensza argomenti
 // che ritorna void. Una server action
 type Props = {
@@ -8,5 +21,28 @@ type Props = {
 };
 
 export function DeleteButton({ action, label = "Elimina" }: Props) {
-  return <button onClick={() => action()}>{label}</button>;
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {/* As a child passale props al button invece di wrapparlo */}
+        <Button variant="destructive" size="sm">
+          {label}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Sei Sicuro?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Questa oporazione non può essere annullata.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Annulla</AlertDialogCancel>
+          <AlertDialogAction onClick={() => action()}>
+            Conferma
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }

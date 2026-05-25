2e868e834f42fe6ff8e32621ace34782739aa02e
Daniele <28843028+Chiaf1@users.noreply.github.com>
2026-05-25 13:29:11 +0200

    removed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div>
-          <h1 className=" text-6xl text-amber-300">Under construction</h1>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Link
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
-            href="/dashboard"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Dashboard page
-          </Link>
-        </div>
-      </main>
-    </div>
-  );
+  redirect("/dashboard");
 }
